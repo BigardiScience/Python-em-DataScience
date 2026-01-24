837a7870b24882eecca7fa2cbe91e6d82da7148f
BigardiScience <guilhermebigardimessias01@gmail.com>
2026-01-24 16:21:50 -0300

    Add files via upload

---
+++ Aula01.py
@@ -0,0 +1,7 @@
+# Analisando instruções da funcao round
+
+num = 2.64
+num2 = 5.75
+soma = num+num2
+
+print(round(soma,1))
---
+++ Aula02.py
@@ -0,0 +1,7 @@
+var_a = 25 
+var_b = 3.14
+var_c = "Python é incrível!"
+var_d = True
+var_e = None
+
+print(f'1 - {var_a} é {type(var_a)}\n',f'2 - {var_b} é {type(var_b)}\n',f'3 - {var_c} é {type(var_c)}\n',f'4 - {var_d} é {type(var_d)}\n',f'5 - {var_e} é {type(var_e)}')
